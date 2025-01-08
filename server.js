8b34e90a42a31d07512209b5ff5a924ece2a1e14
unknown <moraisdecarvalho@gmail.com>
2025-01-08 16:13:03 -0300

    alterações

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', "*");
     res.header("Access-Control-Allow-Headers", "*");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    app.use(cors());
+    app.use(cors({
+        origin: 'https://aafta.vercel.app'
+      }));
     next();
 })
 
