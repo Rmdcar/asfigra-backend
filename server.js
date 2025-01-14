8b0fbf2b900b2f0db5e33834185c68a9ffd36ebc
unknown <moraisdecarvalho@gmail.com>
2025-01-14 15:55:17 -0300

    alterações

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express()
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', "*");
     res.header("Access-Control-Allow-Headers", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
     app.use(cors());
     next();
 })
